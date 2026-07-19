2d72d96ba5b17d47179af5baae7ca1308730a6c7
Vinny <vinny8716@gmail.com>
2026-07-19 16:58:35 -0700

    Version 2.0
    Works

--- dns&ip_check.py
+++ dns&ip_check.py
@@ -1,9 +1,18 @@
 import subprocess
 from network_devices_list import network_devices
-bad_ips = []
-for device in network_devices:
-    if subprocess.call(['ping', '-c', '1', device['Device Address']]) == 0:
-        print('SUCCESS: {device} is reachable'.format(device=device['Device Address']))
+dns_list = ['ns1.d522.wgu.internal', 'ns2.d522.wgu.internal', 'db.d522.wgu.internal', 'api.d522.wgu.internal', 'svr1.d522.wgu.internal', 'svr2.d522.wgu.internal']
+good_ips = [network_devices[2]['Device Address'], network_devices[3]['Device Address'], network_devices[1]['Device Address'], network_devices[0]['Device Address'], network_devices[10]['Device Address'], network_devices[11]['Device Address']]
+i = 0
+bad_dns = []
+dns_temp = ''
+while i < len(dns_list):
+    host = dns_list[i]
+    output = subprocess.check_output(['ping', '-c', '1', host])
+    output = output.decode('utf-8')
+    dns_temp = good_ips[i]
+    if dns_temp in output:
+        print(dns_list[i], 'DNS settings are good')
     else:
-        print('FAILURE: {device} is not reachable'.format(device=device['Device Address']))
-        bad_ips.append(device)
+        print(dns_list[i], 'DNS settings are not working!')
+        bad_dns.append(dns_list[i])
+    i = i + 1
