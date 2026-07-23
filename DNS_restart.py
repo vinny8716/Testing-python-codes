14bea6164fde44e59588038e03b4d71202e9d8bc
Vinny <vinny8716@gmail.com>
2026-07-23 13:36:54 -0700

    DNS restart
    restarts the DNS service on the DNS server

---
+++ DNS_restart.py
@@ -0,0 +1,10 @@
+import paramiko
+from DNS_con import DNS_con
+
+host = ['10.10.10.10', '10.10.10.20']
+command = 'sudo systemctl restart named'
+i = 0
+
+while i < len(host):
+    DNS_con(host[i],command)
+    i = i + 1
