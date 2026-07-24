0a5cebdd1a1d1ccf3ed4dfd271d3f6b2467c50be
Vinny <vinny8716@gmail.com>
2026-07-24 14:52:42 -0700

    update to settings

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ from DNS_con import DNS_con
 from dns_ip_check import bad_dns
 
 
-command = 'sudo resolvectl status'
+command = 'sudo bash -c echo "nameserver 10.10.10.20" > /etc/resolv.conf'
 i = 0
 
 while i < len(bad_dns):
