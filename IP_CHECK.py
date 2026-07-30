4e9dab1d2c4308fef8697a63e2c79b3cd549d85b
Vinny <vinny8716@gmail.com>
2026-07-29 18:50:41 -0700

    Update IP_CHECK.py

--- IP_CHECK.py
+++ IP_CHECK.py
@@ -1,10 +1,11 @@
 import subprocess
+from socket import socket
 from subprocess import DEVNULL
 from network_devices_list import network_devices
 import paramiko
 i = 0
 #ip ping config
-ip_list = ['10.10.10.200', '10.10.10.210', '10.10.10.10', '10.10.10.20' '192.168.10.102', '192.168.20.102', '192.168.30.101', '192.168.10.101', '10.10.10.1', '10.10.10.100', '192.168.20.210', '192.168.30.210']
+ip_list = ['10.10.10.200', '10.10.10.210', '10.10.10.10', '10.10.10.20', '192.168.10.102', '192.168.20.102', '192.168.30.101', '192.168.10.101', '10.10.10.1', '10.10.10.100', '192.168.20.210', '192.168.30.210']
 bad_DNS = []
 device_name = network_devices[i]['Device Name']
 cmd = ['ping', '-c', '1', ip_list[i]]
@@ -14,6 +15,8 @@ username = 'ubuntu'
 password = 'ubuntu'
 command = 'cat /etc/resolv.conf'
 temp_readout = ''
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 #ip ping
 while i < len(ip_list):
@@ -22,18 +25,16 @@ while i < len(ip_list):
     except (subprocess.CalledProcessError, FileNotFoundError):
         print(f'{ip_list[i]} is down!')
     try:
-        client = paramiko.SSHClient()
-        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        client.connect(host = ip_list[i], username = username, password = password, timeout = 5)
+        client.connect(host = ip_list[i], username = username, password = password, timeout = 10)
         stdin, stdout, stderr = client.exec_command(command)
         temp_readout = stdout.read().decode()
-        if '10.10.10.10' or '10.10.10.20' in temp_readout:
-            print(f'{ip_list[i]} is pingable, and DNS settings are right!')
-        else:
+        if '127.0.0.1' in temp_readout:
             ip_list.append(ip_list[i])
-            print(f'{ip_list[i]} is pingable, but DNS settings are wrong!')
-    except Exception as e:
-        print(f'{ip_list[i]} Connection failed!')
+            print(f'{ip_list[i]} is pingable, and DNS settings are wrong!')
+        else:
+            print(f'{ip_list[i]} is pingable, but DNS settings are right!')
+    except (socket.error, paramiko.SSHEexception) as e:
+        print(f'Connection failed: {e}')
     finally:
         client.close()
         i = i + 1
