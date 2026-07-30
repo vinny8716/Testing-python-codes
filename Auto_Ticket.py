95cb177d025c6b2f08a5b2aaefb5931dba14c5ba
Vinny <vinny8716@gmail.com>
2026-07-30 16:03:58 -0700

    Update Auto_Ticket.py

--- Auto_Ticket.py
+++ Auto_Ticket.py
@@ -1,28 +1,23 @@
 import requests
 import json
 from network_devices_list import network_devices
-from dns_ip_check import bad_dns
+from availability_test import bad_ips
 i = 0
 
-while i < len(bad_dns):
-    devie_name = ''
+while i < len(bad_ips):
+    device_name = ['PC3', 'SVR1', 'SVR2']
     token = 'vGkbXkGLqQSo7YLflp9DutuG8st4xdPPF7wnTcwB0FE'
-    for device in network_devices:
-        if bad_dns[i] in device['Device Address']:
-              device_name = device['Device Name']
-        else:
-             pass
     url = 'http://helpdesk.d522.wgu.internal:5000/api/tickets'
     headers = {'Authorization' : f'Bearer {token}',
                'Content-Type' : 'application/json'
                }
     payload = {
-        "assigned_to": "John Smith",
-        "description": "DNS settings are poisoned!",
+        "assigned_to": "John Pork",
+        "description": "Host is down, get the host up and running at the earliest convenience",
         "priority": "high",
         "requester_email": "ITDesk@mailhog.com",
         "status": "open",
-        f"title": "{device_name} is compromised"
+        "title": f"{device_name[i]} is down!"
     }
 
     response = requests.post(url, headers=headers, json=payload)
